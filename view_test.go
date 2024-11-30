5eceabade4382894447e005d80d94c615536a17c
Vitor Barbosa <vitorqb@gmail.com>
2024-11-30 14:24:00 +0100

    feat(placeholders): add placeholder edit

--- view_test.go
+++ view_test.go
@@ -4,6 +4,7 @@ import (
 	"strings"
 	"testing"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	"github.com/stretchr/testify/assert"
 	sazed "github.com/vitorqb/sazed"
 )
@@ -38,9 +39,30 @@ func TestViewCommandEdit(t *testing.T) {
 				Memory: sazed.Memory{Command: "foo {{bar}} baz {{boz}}"},
 			},
 		}
-		model.PlaceholderValues = []string{"--opt1", "--opt2"}
+		for i, value := range []string{"--opt1", "--opt2"} {
+			model.EditTextInputs = append(model.EditTextInputs, textinput.New())
+			model.EditTextInputs[i].SetValue(value)
+		}
 		view := sazed.ViewCommandEdit(model)
 		lines := strings.Split(view, "\n")
 		assert.Equal(t, "Command: foo --opt1 baz --opt2", lines[0])
+	})
+	t.Run("Renders input for each placeholder", func(t *testing.T) {
+		model := sazed.InitialModel(sazed.AppOptions{})
+		model.Matches = []sazed.Match{
+			{
+				Memory: sazed.Memory{Command: "foo {{bar}} baz {{boz}}"},
+			},
+		}
+		model, cmd := sazed.HandleMemorySelected(model)
+		model = batchUpdate(model, cmd)
+		model.EditTextInputs[0].SetValue("--opt1")
+		model.EditTextInputs[1].SetValue("--opt2")
+
+		view := sazed.ViewCommandEdit(model)
+		lines := strings.Split(view, "\n")
+		assert.Equal(t, "Command: foo --opt1 baz --opt2", lines[0])
+		assert.Equal(t, "bar: --opt1 ", lines[1])
+		assert.Equal(t, "boz: --opt2 ", lines[2])
 	})
 }
